89069a2f0c9b403dd60fab326ef5cb1d8a5e851f
Nic Nash <nicnash08@gmail.com>
2015-10-10 11:30:41 -0500

    first working

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+# class SessionsController < ApplicationController
+# end
+
+class SessionsController < Devise::SessionsController
+  respond_to :html, :json
+
+  def create
+    super do |user|
+      if request.format.json?
+        data = {
+          token: user.authentication_token,
+          email: user.email
+        }
+        render json: data, status: 201 and return
+      end
+    end
+  end
+end
