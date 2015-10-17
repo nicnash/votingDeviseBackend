1b322cd57f0ead084acadd5da5ebaf7d8b18bc7b
Nic Nash <nicnash08@Nic-Nashs-iMac.local>
2015-10-16 22:30:00 -0500

    maybe send over idea id too

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,9 +1,9 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :email
   # has_many :ideas
-  # has_many :ideas, embed: :ids, include: true
-  has_many :votes, include: true
-  embed :ids
+  has_many :ideas, embed: :ids #, include: true
+  has_many :votes, embed: :ids, include: true
+  # embed :ids
   #   object.votes.id
   # end
   def say_hello
