c892f07cac06505d0a39531e051864acdd2671aa
Nic Nash <nicnash08@gmail.com>
2015-11-04 20:56:58 -0600

    update token every login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,15 +7,25 @@ class SessionsController < Devise::SessionsController
   def create
     puts '-----------------------create in devise sesions controller'
     super do |user|
-      
+      user.authentication_token = generate_authentication_token
+      user.save
       data = {
         token: user.authentication_token,
         email: user.email
       }
+      
       render json: data, status: 201 and return
       
     end
   end
+
+  private
+    def generate_authentication_token
+      loop do
+        token = Devise.friendly_token
+        break token unless User.where(authentication_token: token).first
+      end
+    end
 
   
 end
